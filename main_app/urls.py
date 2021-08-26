e2f312349692df19180a774cda012efd1dad3419
geedtd <grcazares@gmail.com>
2021-08-25 21:19:55 -0700

    creating trail index model, url, view and template

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.about, name='about'),
+    path('trails/', views.trails_index, name='trails_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 # Define the home view
-def home(request):
-  return HttpResponse('<h1>Let\'s Get Hiking!</h1>')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
     return render(request, 'about.html')
 
+class Trail:
+    def __init__(self, name, description ):
+        self.name = name
+        self.description = description
+
+trails = [
+    Trail('Malibu Creek', 'Beautiful in Malibu'),
+    Trail('Stargazer Trail', 'Hike with beautiful views of the Pacific on the border of San Pedro and PV')
+]
+
+def trails_index(request):
+    return render(request, 'trails/index.html', {'trails': trails})
