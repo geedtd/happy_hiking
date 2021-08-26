6b414bea7068e768fd6098bd71e839dfe1bbe664
geedtd <grcazares@gmail.com>
2021-08-26 01:52:16 -0700

    got a form for adding a trail up

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('trails/', views.trails_index, name='trails_index'),
-    path('trails/<int:trail_id>/', views.trails_detail, name='trails_detail')
+    path('trails/<int:trail_id>/', views.trails_detail, name='trails_detail'),
+    path('trails/create/', views.TrailCreate.as_view(), name='trails_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
 from .models import Trail
 
 # Create your views here.
@@ -28,3 +29,7 @@ def trails_index(request):
 def trails_detail(request, trail_id):
     trail = Trail.objects.get(id=trail_id)
     return render(request, 'trails/detail.html', {'trail': trail})
+
+class TrailCreate(CreateView):
+    model = Trail
+    fields = ['name','length','description',]
